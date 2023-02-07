c482feaa3d1325594d776e52cd1a1e55cae6160b
Marianne Cuevas <mariannecuevas97@gmail.com>
2023-02-07 03:10:24 +0000

    Added a 'submit' event listener to the form element. #1

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var $photoUrl = document.querySelector('.photo-url');
+var $placeholderImage = document.querySelector('.placeholder-img');
+var form = document.querySelector('form');
+
+$photoUrl.addEventListener('input', function (event) {
+  var updateImgUrl = event.target.value;
+  $placeholderImage.setAttribute('src', updateImgUrl);
+});
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var entryData = {};
+  entryData.title = form.elements.title.value;
+  entryData.photoUrl = form.elements.photoUrl.value;
+  entryData.notes = form.elements.notes.value;
+  entryData.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(entryData);
+  $placeholderImage.setAttribute('src', 'images/placeholder-image-square.jpg');
+  form.reset();
+});
